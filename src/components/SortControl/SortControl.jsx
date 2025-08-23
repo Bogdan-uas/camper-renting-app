1a2903d0bb2228ad1ef7493af1d3ddf615d03400
Bogdan_141 <bogdanmakovey1001@gmail.com>
2025-08-23 23:04:34 +0200

    add dropwdown for location filtering, make the map svg unactive when no city filter chosen

--- src/components/SortControl/SortControl.jsx
+++ src/components/SortControl/SortControl.jsx
@@ -1,3 +1,4 @@
+import { useState, useMemo } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import {
     toggleDraftEquipment,
@@ -19,20 +20,44 @@ import Van from '../../assets/icons/type-icons/van.svg?react';
 
 import SortCard from '../reusables/SortCard/SortCard';
 import ReusableButton from '../reusables/ReusableButton/ReusableButton';
+import { selectCampers } from "../../redux/selectors";
 import css from './SortControl.module.css';
 
 const SortControl = () => {
     const dispatch = useDispatch();
     const draft = useSelector(state => state.filters.draft);
+    const campers = useSelector(selectCampers);
+
+    const [showDropdown, setShowDropdown] = useState(false);
+
+    const cities = useMemo(() => {
+        const allLocations = campers.map(c => c.location);
+        return [...new Set(allLocations)].sort();
+    }, [campers]);
+
+    const filteredCities = useMemo(() => {
+        if (!draft.location) return cities;
+        return cities.filter(city =>
+            city.toLowerCase().includes(draft.location.toLowerCase())
+        );
+    }, [cities, draft.location]);
 
     const handleSearch = (e) => {
         e.preventDefault();
         dispatch(resetVisibleCount());
         dispatch(applyFilters());
+        setShowDropdown(false);
     };
 
+    const handleSelectCity = (city) => {
+        dispatch(setDraftLocation(city));
+        setShowDropdown(false);
+    };
+
+    const mapColor = draft.location ? "#101828" : "#6c717b";
+
     return (
-        <form onSubmit={handleSearch}>
+        <form onSubmit={handleSearch} autoComplete="off">
             <div className={css.location_container}>
                 <label className={css.location_label} htmlFor="location">Location</label>
                 <div className={css.location_input_container}>
@@ -43,12 +68,31 @@ const SortControl = () => {
                         type="text"
                         placeholder="City"
                         value={draft.location}
-                        onChange={(e) => dispatch(setDraftLocation(e.target.value))}
+                        onChange={(e) => {
+                            dispatch(setDraftLocation(e.target.value));
+                            setShowDropdown(true);
+                        }}
+                        onFocus={() => setShowDropdown(true)}
+                        onBlur={() => setTimeout(() => setShowDropdown(false), 100)}
                     />
-                    <Map className={css.location_icon} />
+                    <Map
+                        className={css.location_icon}
+                        style={{ color: mapColor }}
+                    />
+                    {showDropdown && filteredCities.length > 0 && (
+                        <ul className={css.dropdown}>
+                            {filteredCities.map(city => (
+                                <li key={city} onClick={() => handleSelectCity(city)}>
+                                    {city}
+                                </li>
+                            ))}
+                        </ul>
+                    )}
                 </div>
             </div>
+
             <span className={css.filters_label}>Filters</span>
+
             <div className={css.filters_container}>
                 <span className={css.filters_vehicle_label}>Vehicle Equipment</span>
                 <div className={css.divider} />
@@ -70,6 +114,7 @@ const SortControl = () => {
                         onChange={() => dispatch(toggleDraftEquipment("Bathroom"))} />
                 </div>
             </div>
+
             <div className={css.filters_container}>
                 <span className={css.filters_vehicle_label}>Vehicle Type</span>
                 <div className={css.divider} />
@@ -85,6 +130,7 @@ const SortControl = () => {
                         onChange={() => dispatch(setDraftVehicleType("Alcove"))} />
                 </div>
             </div>
+
             <ReusableButton
                 text="Search"
                 type="submit"
