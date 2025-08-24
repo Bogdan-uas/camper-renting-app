02c13c4c031c7a8a7bc07b18a83083e44c1035f7
Bogdan_141 <bogdanmakovey1001@gmail.com>
2025-08-24 15:52:28 +0200

    add details card

--- src/components/CamperInfoDetails/CamperDetails/CamperDetails.jsx
+++ src/components/CamperInfoDetails/CamperDetails/CamperDetails.jsx
@@ -1,7 +1,66 @@
-const CamperDetails = () => {
+import css from './CamperDetails.module.css';
+import CategoryCard from '../../reusables/CategoryCard/CategoryCard';
+
+const CamperDetails = ({ camper, features }) => {
+    const formatLabel = (label) => {
+        // Format the label to be sure, every label is capitalized and if it's an abbreviation, it stays uppercase
+        if (label.length <= 3 && label === label.toUpperCase()) {
+            return label;
+        }
+        return label.charAt(0).toUpperCase() + label.slice(1).toLowerCase();
+    };
+    const splitNumberAndUnit = (value) => {
+        if (!value) return { number: value, unit: "" };
+        const match = value.match(/^([\d.]+)([a-zA-Z]+)$/);
+        if (match) {
+            return { number: match[1], unit: match[2] };
+        }
+        return { number: value, unit: "" };
+    };
+
     return (
-        <div>
-            <h2>Details</h2>
+        <div className={css.details_container}>
+            <ul className={css.category_list}>
+                {features.map((feature, idx) => (
+                    <li key={idx}>
+                        <CategoryCard icon={feature.icon} label={feature.label} />
+                    </li>
+                ))}
+            </ul>
+            <div className={css.vehicle_details_container}>
+                <span className={css.vehicle_details}>Vehicle Details</span>
+                <div className={css.divider} />
+                <div className={css.vehicle_detail_info}>
+                    {/* This whole structure is in order to follow the DRY-method and make the value like 123 m (and like 123m) */}
+                    {camper?.form && (
+                        <span className={css.vehicle_detail_wrapper}>
+                            <span className={css.vehicle_detail_label}>Form</span>
+                            <span className={css.vehicle_detail_label}>{formatLabel(camper.form)}</span>
+                        </span>
+                    )}
+                    {["length", "width", "height", "tank"].map((key) => {
+                        if (!camper?.[key]) return null;
+                        const { number, unit } = splitNumberAndUnit(camper[key]);
+                        return (
+                            <span key={key} className={css.vehicle_detail_wrapper}>
+                                <span className={css.vehicle_detail_label}>
+                                    {key.charAt(0).toUpperCase() + key.slice(1)}
+                                </span>
+                                <span className={css.vehicle_detail_label}>
+                                    {number} {unit}
+                                </span>
+                            </span>
+                        );
+                    })}
+                    {camper?.consumption && (
+                        <span className={css.vehicle_detail_wrapper}>
+                            <span className={css.vehicle_detail_label}>Consumption</span>
+                            <span className={css.vehicle_detail_label}>{camper.consumption}</span>
+                        </span>
+                    )}
+                    {/* This whole structure is in order to follow the DRY-method and make the value like 123 m (and like 123m) */}
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/CamperInfoDetails/CamperInfoDetails.jsx
+++ src/components/CamperInfoDetails/CamperInfoDetails.jsx
@@ -1,7 +1,24 @@
 import { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setTab } from "../../redux/ui/camperTabSlice";
-import { selectActiveTab } from "../../redux/selectors";
+import {
+    selectActiveTab,
+    selectCamperById,
+} from "../../redux/selectors";
+import { useParams } from "react-router-dom";
+
+import AC from '../../assets/icons/category-icons/ac.svg?react';
+import Cup from '../../assets/icons/category-icons/cup.svg?react';
+import Engine from '../../assets/icons/category-icons/engine.svg?react';
+import Fridge from '../../assets/icons/category-icons/fridge.svg?react';
+import Gas from '../../assets/icons/category-icons/gas.svg?react';
+import Microwave from '../../assets/icons/category-icons/microwave.svg?react';
+import Radio from '../../assets/icons/category-icons/radio.svg?react';
+import Shower from '../../assets/icons/category-icons/shower.svg?react';
+import Transmission from '../../assets/icons/category-icons/transmission.svg?react';
+import TV from '../../assets/icons/category-icons/tv.svg?react';
+import Water from '../../assets/icons/category-icons/water.svg?react';
+
 
 import css from './CamperInfoDetails.module.css';
 import CamperDetails from './CamperDetails/CamperDetails';
@@ -9,9 +26,26 @@ import CamperReviews from './CamperReviews/CamperReviews';
 
 const CamperInfoDetails = () => {
     const dispatch = useDispatch();
+    const { id } = useParams();
+
+    const camper = useSelector(selectCamperById(id));
     const activeTab = useSelector(selectActiveTab);
     const linksRef = useRef(null);
     const [underlineStyle, setUnderlineStyle] = useState({ width: 0, transform: 'translateX(0px)' });
+
+    const features = camper ? [
+        camper.AC && { icon: AC, label: "AC" },
+        camper.kitchen && { icon: Cup, label: "Kitchen" },
+        camper.engine && { icon: Engine, label: camper.engine },
+        camper.refrigerator && { icon: Fridge, label: "Fridge" },
+        camper.gas && { icon: Gas, label: "Gas" },
+        camper.microwave && { icon: Microwave, label: "Microwave" },
+        camper.radio && { icon: Radio, label: "Radio" },
+        camper.bathroom && { icon: Shower, label: "Bathroom" },
+        camper.transmission && { icon: Transmission, label: camper.transmission },
+        camper.TV && { icon: TV, label: "TV" },
+        camper.water && { icon: Water, label: "Water" },
+    ].filter(Boolean) : [];
 
     useEffect(() => {
         const buttons = linksRef.current.children;
@@ -29,12 +63,14 @@ const CamperInfoDetails = () => {
                     <button
                         className={css.tab_button}
                         onClick={() => dispatch(setTab("details"))}
+                        disabled={activeTab === "details"}
                     >
                         Features
                     </button>
                     <button
                         className={css.tab_button}
                         onClick={() => dispatch(setTab("reviews"))}
+                        disabled={activeTab === "reviews"}
                     >
                         Reviews
                     </button>
@@ -44,7 +80,9 @@ const CamperInfoDetails = () => {
             <div className={css.divider} />
 
             <div className={css.tab_content}>
-                {activeTab === "details" && <CamperDetails />}
+                {activeTab === "details" &&
+                    <CamperDetails camper={camper} features={features} />
+                }
                 {activeTab === "reviews" && <CamperReviews />}
             </div>
         </div>
